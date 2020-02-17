5d5818fedf514ff50f0e62a671be68449dce6e83
Jathusanth <jathusanth11@gmail.com>
2020-02-17 14:45:39 +1000

    no matter what do not quit

--- ToolsSL/Brand.Designer.cs
+++ ToolsSL/Brand.Designer.cs
@@ -18,6 +18,8 @@
                 components.Dispose();
             }
             base.Dispose(disposing);
+
+            /// adding comment 
         }
 
         #region Windows Form Designer generated code
